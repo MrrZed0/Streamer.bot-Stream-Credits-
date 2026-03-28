8480e3735c933593bbb01ec029732f73d2f53c8f
MrrZed0 <154046655+MrrZed0@users.noreply.github.com>
2026-03-28 10:52:24 -0400

    Add files via upload

---
+++ Ending_Stream/credits_data.js
@@ -0,0 +1,7 @@
+﻿window.CREDITS_DATA = [
+  {"type":"gifted","userName":"mrwhippy76","profileImage":"https://static-cdn.jtvnw.net/jtv_user_pictures/22cbb696-ff8a-461c-8445-5e47a5b7fe22-profile_image-300x300.jpeg","detail":"Gifted 1 Subs","recipients":["MrBeast6000"]},
+  {"type":"follow","userName":"Stefxx96","profileImage":"https://static-cdn.jtvnw.net/jtv_user_pictures/1691d15c-dd94-4bef-acab-1ae6726a709d-profile_image-300x300.png","detail":"New Follower","recipients":[]},
+  {"type":"follow","userName":"mrwhippy76","profileImage":"https://static-cdn.jtvnw.net/jtv_user_pictures/22cbb696-ff8a-461c-8445-5e47a5b7fe22-profile_image-300x300.jpeg","detail":"New Follower","recipients":[]},
+  {"type":"follow","userName":"dababyy1421","profileImage":"https://static-cdn.jtvnw.net/jtv_user_pictures/b215255c-788c-4459-a6ea-f9935a2d906e-profile_image-300x300.png","detail":"New Follower","recipients":[]},
+  {"type":"gifted","userName":"dababyy1421","profileImage":"https://static-cdn.jtvnw.net/jtv_user_pictures/b215255c-788c-4459-a6ea-f9935a2d906e-profile_image-300x300.png","detail":"Gifted 1 Subs","recipients":["Stefxx96"]}
+];
